7657a79dd8d047a0c6efd236361ea7f60e200384
Nikita <nikita@nikita-2.local>
2013-12-12 18:00:39 +0530

    Adding the installation script

---
+++ mongodb/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#Import the MongoDB public GPG key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+#Add the 10gen package to get the latest stable mongodb release
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+#Reload your repository
+sudo apt-get update
+
+#Install MongoDB
+sudo apt-get install mongodb-10gen
